eadf18429d229584b804486b2edb5089b4d98108
manepriyanka2014 <manepriyanka2014@gmail.com>
2021-10-22 23:48:02 +0530

    Append data at End Linked List

--- LinkedListMain.java
+++ LinkedListMain.java
@@ -1,23 +1,20 @@
 package com.bridgelabz.linkedlist;
 
+import java.util.Scanner;
+
 public class LinkedListMain {
-
-	public static void main(String[] args) {
-		System.out.println("Welcome to Linked List");
-
-		Node firstNode = new Node(56);
-		Node secondNode = new Node(70);
-		Node thirdNode = new Node(30);
-
-		Node head = firstNode;
-		firstNode.next = secondNode;
-		secondNode.next = thirdNode;
-		Node tail = thirdNode;
-
-		Node temp = head;
-		while (temp != null) {
-			System.out.println(temp.data);
-			temp = temp.next;
-		}
-	}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter 1 to add the data at the start\n" +
+                            "Enter 2 to add the data at the end");
+        switch(scanner.nextInt()) {
+            case 1:
+                Operation.addDataAtStart();
+                break;
+            case 2:
+                Operation.addDataAtEnd();
+                break;
+        }
+        scanner.close();
+    }
 }
