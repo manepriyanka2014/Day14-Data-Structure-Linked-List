80880601322de9a57938b0948fa529108720d612
manepriyanka2014 <manepriyanka2014@gmail.com>
2021-10-22 23:58:09 +0530

    inserted data between two nodes

--- LinkedList.java
+++ LinkedList.java
@@ -1,11 +1,10 @@
 package com.bridgelabz.linkedlist;
 
 public class LinkedList {
-
 	Node head;
 	Node tail;
 
-	public void push(int data) {
+	public Node push(int data) {
 		Node newNode = new Node(data);
 		if (head == null) {
 			head = newNode;
@@ -15,6 +14,7 @@ public class LinkedList {
 			this.head = newNode;
 			newNode.next = temp;
 		}
+		return newNode;
 	}
 
 	public void append(int data) {
@@ -29,6 +29,12 @@ public class LinkedList {
 		}
 	}
 
+	public void insertInBetween(Node previousNode, Node newNode) {
+		Node temp = previousNode.next;
+		previousNode.next = newNode;
+		newNode.next = temp;
+	}
+
 	public void print() {
 		if (head == null) {
 			System.out.println("Linked List is Empty");
@@ -38,9 +44,10 @@ public class LinkedList {
 				if (temp.next != null) {
 					System.out.print(temp.data + " -> ");
 					temp = temp.next;
-				} else
+				} else {
 					System.out.print(temp.data + " -> ");
-				temp = temp.next;
+					temp = temp.next;
+				}
 			}
 		}
 	}
--- Operation.java
+++ Operation.java
@@ -2,19 +2,29 @@ package com.bridgelabz.linkedlist;
 
 public class Operation {
 
-	public static void addDataAtStart() {
-		LinkedList linkedList = new LinkedList();
+	LinkedList linkedList = new LinkedList();
+
+	public void addDataAtStart() {
 		linkedList.push(70);
 		linkedList.push(30);
 		linkedList.push(56);
 		linkedList.print();
 	}
 
-	public static void addDataAtEnd() {
-		LinkedList linkedList = new LinkedList();
+	public void addDataAtEnd() {
 		linkedList.append(56);
 		linkedList.append(30);
 		linkedList.append(70);
+		linkedList.print();
+	}
+
+	public void addDataAtMiddle() {
+		Node secondNode = linkedList.push(70);
+		Node firstNode = linkedList.push(56);
+		Node newNode = new Node(30);
+		linkedList.print();
+		System.out.println("");
+		linkedList.insertInBetween(firstNode, newNode);
 		linkedList.print();
 	}
 }
