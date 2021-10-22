654c904b36576888d76947082b946029522ef370
manepriyanka2014 <manepriyanka2014@gmail.com>
2021-10-22 22:48:35 +0530

    Created Simple Linked List

--- LinkedListMain.java
+++ LinkedListMain.java
@@ -4,5 +4,20 @@ public class LinkedListMain {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Linked List");
+
+		Node firstNode = new Node(56);
+		Node secondNode = new Node(70);
+		Node thirdNode = new Node(30);
+
+		Node head = firstNode;
+		firstNode.next = secondNode;
+		secondNode.next = thirdNode;
+		Node tail = thirdNode;
+
+		Node temp = head;
+		while (temp != null) {
+			System.out.println(temp.data);
+			temp = temp.next;
+		}
 	}
 }
